5ddcddae9d25179dbcee47d1a36f585848efe2f2
VytenisDom <vytenis.domasevicius@vka.lt>
2019-02-10 21:36:07 +0200

    Working prototype, all the scraping and converting is done

--- WebScraper/Form1.cs
+++ WebScraper/Form1.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,65 +23,138 @@ namespace WebScraper
         private void InitTable()
         {
             table = new DataTable("GameRankingsDataTable");
-            table.Columns.Add("Name", typeof(string));
-            table.Columns.Add("Score", typeof(string));
-            table.Columns.Add("NumReviews", typeof(string));
+            table.Columns.Add("Title", typeof(string));
+            table.Columns.Add("ListingPrice", typeof(string));
+            table.Columns.Add("RealPrice", typeof(string));
+            table.Columns.Add("Profit", typeof(string));
             dataGridView1.DataSource = table;
+            dataGridView1.Columns[0].Width = 200;
         }
 
-        private async Task<List<GameReview>> GameRankingsFromPage(int pageNum)
+        private async Task<List<Item>> ListingsFromPage(int pageNum)
         {
-            string url = @"https://backpack.tf/classifieds";
+            string url = @"https://backpack.tf/classifieds?page="+ pageNum.ToString() +"&slot=misc&quality=5";
             
             var doc = await Task.Factory.StartNew(() => web.Load(url));
-            var noderrrr = doc.DocumentNode.SelectSingleNode("//head/title");
-            var nameNodes = doc.DocumentNode.SelectNodes("//*[@id=\"page-content\"]/div[4]/div[1]/div/div[2]/ul");
-            string[] ids = new string[11];
+            var idNodes = doc.DocumentNode.SelectNodes("//*[@id=\"page-content\"]/div[4]/div[1]/div/div[2]/ul");
+            List<string> ids = new List<string>();
+            List<string> prices = new List<string>();
+            List<string> titles = new List<string>();
+            List<string> effects = new List<string>();
+            List<string> bptfPrices = new List<string>();
             var id = 0;
-            foreach (var nNode in nameNodes.Descendants("li"))
+            foreach (var liNode in idNodes.Descendants("li"))
             {
-                if (nNode.NodeType == HtmlNodeType.Element)
-                {
-                    ids[id] = nNode.GetAttributeValue("id", "").ToString();
-                    id++;
+                ids.Add(liNode.GetAttributeValue("id", "").ToString());
+                try{
+                    bptfPrices.Add(doc.DocumentNode.SelectSingleNode(" //*[@id=\"" + ids[id] + "\"]/div[1]/div").Attributes["data-p_bptf"].Value);
+                } catch {
+                    bptfPrices.Add("-");
                 }
+                prices.Add(doc.DocumentNode.SelectSingleNode(" //*[@id=\"" + ids[id] + "\"]/div[1]/div").Attributes["data-listing_price"].Value);
+                titles.Add(doc.DocumentNode.SelectSingleNode(" //*[@id=\"" + ids[id] + "\"]/div[1]/div").Attributes["data-base_name"].Value);
+                effects.Add(doc.DocumentNode.SelectSingleNode(" //*[@id=\"" + ids[id] + "\"]/div[1]/div").Attributes["data-effect_name"].Value);
+                id++;
             }
 
-
-            var scoreNodes = doc.DocumentNode.SelectNodes("//*[@id=\"page-content\"]/div[3]/div[1]/div/div[1]/span[1]");
-            var numReviewNodes = doc.DocumentNode.SelectNodes("//*[@id=\"page-content\"]/div[3]/div[1]/div/div[1]/span[1]");
-            //If these are null it means the name/score nodes couldn't be found on the html page
-            if (nameNodes == null || scoreNodes == null || numReviewNodes == null)
-                return new List<GameReview>();
-
-            var names = nameNodes.Select(node => node.InnerText).ToList();
-            var scores = scoreNodes.Select(node => node.InnerText).ToList();
-            var numReviews = numReviewNodes.Select(node => node.LastChild.InnerText).ToList();
-
-            List<GameReview> toReturn = new List<GameReview>();
-            for (int i = 0; i < names.Count(); ++i)
-                toReturn.Add(new GameReview() { Name = names[i], Score = scores[i], NumReviews = numReviews[i] });
-
+            List<Item> toReturn = new List<Item>();
+            for (int i = 0; i < ids.Count(); ++i)
+                toReturn.Add(new Item() { HatBaseName = titles[i], AdPrice = prices[i], RealPrice = bptfPrices[i], Effect = effects[i] });
+                
             return toReturn;
         }
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-            int pageNum = 0;
-            var rankings = await GameRankingsFromPage(0);
-            while (rankings.Count > 0)
-            {
-                foreach (var ranking in rankings)
-                    table.Rows.Add(ranking.Name, ranking.Score, ranking.NumReviews);
+            int pageNum = 1;
+            var Listings = await ListingsFromPage(1);
+            while (Listings.Count > 0){
+                foreach (var listing in Listings){
+                    string fullName = listing.Effect + " " + listing.HatBaseName;
+                    double finalAdPrice = finalPrice(listing.AdPrice);
+                    double finalRealPrice = finalPrice(listing.RealPrice);
+                    string profitMargin = profit(finalAdPrice, finalRealPrice);
+                    table.Rows.Add(fullName, finalAdPrice, finalRealPrice, profitMargin);
+                }
                 pageNum++;
-                rankings = await GameRankingsFromPage(pageNum);
+                Listings = await ListingsFromPage(pageNum);
             }
         }
+
+        public static string profit(double Ad, double Real)
+        {
+            if(Real != 0){
+                double b = Math.Round(Real - Ad, 2);
+                return b.ToString();
+            } else {
+                return "Unknown";
+            }
+           
+        }
+
+        public static double finalPrice(string str)
+        {
+            double ratio = 44.11;
+            double final = 0;
+            double amount_in_keys = 0;
+            double amount_in_ref = 0;
+            if (str.Contains("–"))
+            {
+                int index = str.IndexOf("–");
+                final = (getDouble(str.Substring(0, index)) + getDouble(str.Substring(index + 1))) / 2;
+
+            }
+            else if (str.Contains("keys") && str.Contains("ref"))
+            {
+                int index = str.IndexOf("keys");
+                index += 5;
+                amount_in_keys = getDouble(str.Substring(0, index));
+                amount_in_ref = getDouble(str.Substring(index));
+                final = amount_in_keys + convert(amount_in_ref, ratio);
+            }
+            else if (str.Contains("keys"))
+            {
+                final = getDouble(str);
+            }
+            else
+            {
+                final = convert(getDouble(str), ratio);
+            }
+
+            return final;
+        }
+        public static double getDouble(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || c == ' ' || c == '.')
+                {
+                    sb.Append(c);
+                }
+                else break;
+            }
+            try
+            {
+                return Double.Parse(sb.ToString(), CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return 0;
+            }
+           
+        }
+        public static double convert(double refined, double ratio)
+        {
+            return Math.Round(refined / ratio, 2);
+        }
     }
-    public class GameReview
+    public class Item
     {
-        public string Name { get; set; }
-        public string Score { get; set; }
-        public string NumReviews { get; set; }
+        public string HatBaseName { get; set; }
+        public string AdPrice { get; set; }
+        public string RealPrice { get; set; }
+        public string Effect { get; set; }
+        public string Link { get; set; }
     }
 }
