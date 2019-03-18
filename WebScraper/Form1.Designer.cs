982bd929a7018ab3df02dfd06355c4abbb6376c7
VytenisDom <vytenis.domasevicius@vka.lt>
2019-03-18 16:08:42 +0200

    Now works for servers, added an option to choose budget

--- WebScraper/Form1.Designer.cs
+++ WebScraper/Form1.Designer.cs
@@ -37,6 +37,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,7 +58,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LimeGreen;
-            this.button1.Font = new System.Drawing.Font("Adobe Garamond Pro", 9.749999F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(683, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(67, 42);
@@ -85,7 +87,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(683, 96);
+            this.label2.Location = new System.Drawing.Point(683, 174);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(93, 13);
             this.label2.TabIndex = 4;
@@ -93,7 +95,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(686, 149);
+            this.textBox2.Location = new System.Drawing.Point(686, 112);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(34, 20);
             this.textBox2.TabIndex = 5;
@@ -102,17 +104,36 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(683, 133);
+            this.label3.Location = new System.Drawing.Point(683, 96);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Start from page";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(683, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Maximum Budget ( in keys )";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(686, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(32, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "20";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(849, 635);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
@@ -138,6 +159,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- WebScraper/Form1.cs
+++ WebScraper/Form1.cs
@@ -24,8 +24,8 @@ namespace WebScraper
         {
             table = new DataTable("GameRankingsDataTable");
             table.Columns.Add("Title", typeof(string));
-            table.Columns.Add("ListingPrice", typeof(string));
-            table.Columns.Add("RealPrice", typeof(string));
+            table.Columns.Add("ListingPrice", typeof(double));
+            table.Columns.Add("RealPrice", typeof(double));
             table.Columns.Add("Potencial ROI", typeof(double));
             table.Columns.Add("ROI in %", typeof(double));
             dataGridView1.DataSource = table;
@@ -34,7 +34,13 @@ namespace WebScraper
 
         private async Task<List<Item>> ListingsFromPage(int pageNum)
         {
-            string url = @"https://backpack.tf/classifieds?page="+ pageNum.ToString() +"&slot=misc&quality=5";
+            string url = @"https://backpack.tf/classifieds?page="+ pageNum.ToString() + "&quality=5&slot=misc";
+            //&particle=3004%2C91%2C59%2C3017%2C98%2C82%2C81%2C19%2C4%2C38%2C75%2C30%2C58%2C121%2C39%2C92%2C73%2C13%2C11%2C69%2C10
+            //6 %2C60%2C90%2C94%2C67%2C100%2C80%2C704%2C103%2C62%2C703%2C111%2C34%2C105%2C40%2C37%2C79%2C18%2C3018%2C8%2C3003%2C10
+            //2 %2C701%2C3015%2C85%2C3005%2C3012%2C45%2C3013%2C101%2C119%2C115%2C84%2C70%2C17%2C57%2C118%2C110%2C77%2C109%2C93%2C64
+            //%2C83%2C29%2C47%2C116%2C96%2C16%2C108%2C36%2C3014%2C3001%2C88%2C12%2C89%2C3010%2C28%2C3016%2C9%2C113%2C46%2C95%2C44%2
+            //C63 %2C3022%2C10%2C15%2C3006%2C7%2C31%2C66%2C107%2C3008%2C43%2C56%2C61%2C104%2C32%2C68%2C14%2C35%2C3007%2C3021%2C72%2
+            //C3019 %2C65%2C33%2C99%2C112%2C120%2C76%2C117%2C3020%2C74%2C3009%2C114%2C78%2C71%2C97%2C86%2C87%2C6%2C702%2C3011
             label2.Text = "Currently indexing page " + pageNum.ToString();
             var doc = await Task.Factory.StartNew(() => web.Load(url));
             var idNodes = doc.DocumentNode.SelectNodes("//*[@id=\"page-content\"]/div[4]/div[1]/div/div[2]/ul");
@@ -54,13 +60,14 @@ namespace WebScraper
                 }
                 prices.Add(doc.DocumentNode.SelectSingleNode(" //*[@id=\"" + ids[id] + "\"]/div[1]/div").Attributes["data-listing_price"].Value);
                 titles.Add(doc.DocumentNode.SelectSingleNode(" //*[@id=\"" + ids[id] + "\"]/div[1]/div").Attributes["data-base_name"].Value);
+                // Crashina ant unusilifieriu ir effect
                 effects.Add(doc.DocumentNode.SelectSingleNode(" //*[@id=\"" + ids[id] + "\"]/div[1]/div").Attributes["data-effect_name"].Value);
                 id++;
             }
 
             List<Item> toReturn = new List<Item>();
             for (int i = 0; i < ids.Count(); ++i)
-                toReturn.Add(new Item() { HatBaseName = titles[i], AdPrice = prices[i], RealPrice = bptfPrices[i], Effect = effects[i] });
+                toReturn.Add(new Item() {ID = ids[i], HatBaseName = titles[i], AdPrice = prices[i], RealPrice = bptfPrices[i], Effect = effects[i] });
                 
             return toReturn;
         }
@@ -146,6 +153,7 @@ namespace WebScraper
             return Math.Round(refined / ratio, 2);
         }
         int pageNum = 0;
+        List<string> GreatDealIDs = new List<string>();
         private async void timer1_Tick(object sender, EventArgs e)
         {
             pageNum++;
@@ -157,11 +165,23 @@ namespace WebScraper
                 double finalRealPrice = finalPrice(listing.RealPrice);
                 string profitMargin = profit(finalAdPrice, finalRealPrice);
                 double profitPercent = profitPercentage(finalAdPrice, finalRealPrice);
-                //if(Double.Parse(profitPercent) > 0)
-                table.Rows.Add(fullName, finalAdPrice, finalRealPrice, profitMargin, profitPercent);
+                if (profitPercent > 25){
+
+                    if(finalAdPrice <= maxBudget){
+
+                        if(!GreatDealIDs.Contains(listing.ID)){
+                            table.Rows.Add(fullName, finalAdPrice, finalRealPrice, profitMargin, profitPercent);
+                            GreatDealIDs.Add(listing.ID);
+                        }
+                    } else {
+                        //Viskas is naujo, ieskom nauju
+                        pageNum = int.Parse(textBox2.Text);
+                    }
+                }   
             }
         }
         bool on = false;
+        double maxBudget = 0;
         private void button1_Click(object sender, EventArgs e)
         {
             if (!on) {
@@ -171,8 +191,10 @@ namespace WebScraper
                 button1.Text = "STOP";
                 timer1.Interval = int.Parse(textBox1.Text);
                 pageNum = int.Parse(textBox2.Text);
+                maxBudget = double.Parse(textBox3.Text);
                 textBox1.Enabled = false;
                 textBox2.Enabled = false;
+                textBox3.Enabled = false;
             } else {
                 timer1.Enabled = false;
                 on = false;
@@ -181,11 +203,13 @@ namespace WebScraper
                 textBox2.Text = pageNum.ToString();
                 textBox1.Enabled = true;
                 textBox2.Enabled = true;
+                textBox3.Enabled = true;
             }
         }
     }
     public class Item
     {
+        public string ID { get; set; }
         public string HatBaseName { get; set; }
         public string AdPrice { get; set; }
         public string RealPrice { get; set; }
